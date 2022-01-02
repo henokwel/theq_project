9731c3fb79ab16465212633c2514530a1cbecdd0
henokwel <henokwel@outlook.com>
2022-01-02 04:01:08 +0100

    fixed toggle fn and other toggle related state issue

--- pages/quizbuilder.js
+++ pages/quizbuilder.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import {
-    ToggleButton, ToggleButtonGroup, Breadcrumbs, Link as M_Link, Typography
+    ToggleButton, ToggleButtonGroup, Breadcrumbs, Link as M_Link, Typography, InputLabel, Select, MenuItem, FormHelperText
 } from '@mui/material'
 
 
@@ -35,14 +35,16 @@ const uniqid = require("uniqid");
 
 const Quizbuilder = () => {
     const [currentQ, setCurrentQ] = useState("title")
+    const [started, setStarted] = useState(false) // Toggle Display avilability controller
+
 
     const [qInfo, setQInfo] = useState({
         id: uniqid(),
         title: "",
         desc: "",
-        createdDate: "",
+        createdDate: new Date().toISOString().substring(0, 10),
         deadline: "",
-        participants: { goal: 100, current: "" }
+        participants: ""
     })
     const [qArr, setQArr] = useState([
         {
@@ -66,11 +68,109 @@ const Quizbuilder = () => {
     const _quizContextUpdate = useContext(QuizUpdateContext)
 
     const handleAlignment = (event, newAlignment) => {
-        console.log(newAlignment);
+        console.log("new", newAlignment);
+        console.log("current", currentQ);
+        if (newAlignment === null) return
+
         setCurrentQ(newAlignment)
+        console.log(qArr);
         // setAlignment(newAlignment);
     };
 
+
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
+
+    const handelFinish = async () => {
+
+        // 1) Make sure the last Q in array isn't empty. 
+        // If it's remove it!
+
+
+        // 2) Handle Started state ON start & On Finish
+
+        if (!started) {
+            setStarted(true)
+            handleAlignment(null, qArr[0].id)
+        }
+
+
+        // dispatch quiz arr 
+        // _quizContextUpdate({ type: "add", payload: qArr })
+        // redirect to dashbord
+
+
+
+    }
+
+
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        router.push("/")
+        console.log(qArr);
+        // Check for unfinished quiz and prompt if the user want to discard or draft it,
+        // before redirect to dashbord
+
+        // if (qArr.length >= 2) {
+
+        // handleClickOpen()
+        // }
+
+        console.info('You clicked a breadcrumb.');
+    }
+
+
+    // ================= Init Quiz Form handler ==================== //
+
+    const handleIntroInputChange = (e) => {
+        const values = { ...qInfo }
+
+        const target = e.target
+
+
+        if (target.name === "title" && target.value !== " ") {
+            values.title = target.value;
+        } else if (target.name === "description" && target.value !== " ") {
+            values.desc = target.value;
+        } else if (target.name === "deadline") {
+            values.deadline = target.value;
+        } else if (target.name === "participants" && target.value !== " ") {
+            values.participants = target.value;
+        }
+
+        setQInfo(values)
+
+        console.log(qInfo);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ================= QUIZ  Form handler ==================== //
 
     const handleInputChange = (
         index,
@@ -94,10 +194,6 @@ const Quizbuilder = () => {
         else if (target.name === "answer") {
             values[index].answer = target.value;
         }
-        // else {
-        //     values[index].answer = target.value;
-        // }
-
 
         setQArr(values);
     };
@@ -132,9 +228,15 @@ const Quizbuilder = () => {
         else {
             console.log("Last Else");
 
+
+            // Get the current ID  
+            // For toggle purposes
+            let newId = uniqid()
+
+
             if (lastQ.answer) {
                 values.push({
-                    id: uniqid(),
+                    id: newId,
                     q: "",
                     answer: null,
                     altA: "",
@@ -142,40 +244,18 @@ const Quizbuilder = () => {
                     altC: "",
                 });
                 setInputError({ id: lastQ.id, q: false, alt: false, answer: false });
+                handleAlignment(null, newId)
                 setQArr(values);
                 // return
             } else {
                 setInputError({ id: lastQ.id, q: false, alt: false, answer: true });
             }
         }
-        console.log(qArr);
 
     };
 
 
-    const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-
-    const handelFinish = async () => {
-
-        // Make sure the last Q in array isn't empty. 
-        // If it's remove it!
-
-        // dispatch quiz arr 
-        _quizContextUpdate({ type: "add", payload: qArr })
-        // redirect to dashbord
-
-
-
-    }
 
 
     const handleRemoveFields = (index) => {
@@ -184,23 +264,10 @@ const Quizbuilder = () => {
         setQArr(values);
     };
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        router.push("/")
-        console.log(qArr);
-        // Check for unfinished quiz and prompt if the user want to discard or draft it,
-        // before redirect to dashbord
 
-        // if (qArr.length >= 2) {
-
-        // handleClickOpen()
-        // }
-
-        console.info('You clicked a breadcrumb.');
-    }
+    console.log(qInfo);
 
 
- 
     return (
         <>
             <CssBaseline />
@@ -238,8 +305,8 @@ const Quizbuilder = () => {
                         </ToggleButton>
 
                         {
-                            qArr.map(item => <ToggleButton disabled={qInfo.title === ""} key={item.id} value={item.id} aria-label="centered">
-                                <h3 style={{ margin: 0 }}>Q1</h3>
+                            qArr.map((item, index) => <ToggleButton sx={{ visibility: started ? "visible" : "hidden" }} key={item.id} value={item.id} aria-label="centered">
+                                <h3 style={{ margin: 0 }}>{`Q${index + 1}`}</h3>
                             </ToggleButton>
 
                             )
@@ -281,17 +348,7 @@ const Quizbuilder = () => {
                 </div>
 
 
-
-
-                {/* 
-                id: uniqid(),
-        title: "",
-        desc: "",
-        createdDate: "",
-        deadline: "",
-        participants: { goal: 100, current: "" }
-                { */}
-
+                {/* // ===== Intro Form  ========= // */}
                 {
 
                     <Box sx={{ m: 2, display: currentQ === "title" ? "block" : "none" }} key={`${qInfo.id}`}  >
@@ -302,11 +359,10 @@ const Quizbuilder = () => {
                                 id="outlined-multiline-flexible"
                                 name="title"
                                 value={qInfo.title}
-                                // onChange={(e) => handleInputChange(null, e)}
+                                onChange={(e) => handleIntroInputChange(e)}
                                 placeholder="Quiz Title"
                                 multiline
                                 fullWidth
-                                required
                                 // error={qInfo.id === inputError.id ? inputError.q : false}
                                 maxRows={4}
 
@@ -318,85 +374,56 @@ const Quizbuilder = () => {
 
                             <TextField
                                 id="outlined-multiline-flexible"
-                                name="title"
-                                value={qInfo.title}
-                                // onChange={(e) => handleInputChange(index, e)}
+                                name="description"
+                                value={qInfo.desc}
+                                onChange={(e) => handleIntroInputChange(e)}
                                 placeholder="Short description"
                                 multiline
                                 fullWidth
                                 // error={qInfo.id === inputError.id ? inputError.q : false}
                                 minRows={5}
                             />
-
-                            <TextField
-                                id="date"
-                                label="Deadline"
-                                type="date"
-                                defaultValue={`${new Date().toISOString().substring(0, 10).toString()}`}
-                                sx={{ width: 220, mt: 4 }}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
+                            <div>
 
 
-                            {/* Multiple choice */}
-
-                            {/* <TextField
-                                id="outlined-multiline-flexible"
-                                placeholder="Enter choose A"
-                                name="altA"
-                                // error={q.id === inputError.id ? inputError.alt : false}
-                                value={q.altA}
-                                onChange={e => handleInputChange(index, e)}
-                            /> */}
-                            {/* <Stack spacing={1} sx={{ minWidth: "80%" }}>
-
-                            <TextField
-                                id="outlined"
-                                placeholder="Enter choose B "
-                                name="altB"
-                                error={q.id === inputError.id ? inputError.alt : false}
-                                value={q.altB}
-                                onChange={e => handleInputChange(index, e)}
-                            />
-
-                            <TextField
-                                id="outlined"
-                                placeholder="Enter choose C "
-                                name="altC"
-                                error={q.id === inputError.id ? inputError.alt : false}
-                                value={q.altC}
-                                onChange={e => handleInputChange(index, e)}
-                            />
-                        </Stack> */}
+                                <TextField
+                                    id="date"
+                                    label="Deadline"
+                                    type="date"
+                                    name='deadline'
+                                    onChange={(e) => handleIntroInputChange(e)}
+                                    value={qInfo.deadline}
+                                    defaultValue={`${new Date().toISOString().substring(0, 10).toString()}`}
+                                    sx={{ width: 220, mt: 4 }}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
 
 
+                                <FormControl sx={{ mt: 4, ml: 5, minWidth: 120 }}>
+                                    <InputLabel id="demo-simple-select-helper-label">Participants</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-helper-label"
+                                        id="demo-simple-select-helper"
+                                        value={qInfo.participants}
+                                        label="participants"
+                                        name="participants"
+                                        onChange={(e) => handleIntroInputChange(e)}
+                                    >
+                                        <MenuItem value="">
+                                            <em>None</em>
+                                        </MenuItem>
+                                        <MenuItem value={10}>10</MenuItem>
+                                        <MenuItem value={100}>100</MenuItem>
+                                        <MenuItem value={200}>200</MenuItem>
+                                    </Select>
+                                    <FormHelperText>Estimate participants </FormHelperText>
+                                </FormControl>
 
-                            {/* Answer */}
-                            {/* <FormControl component="fieldset" error={true}>
-                            <RadioGroup
-                                aria-label="select correct answer"
-                                name="answer"
-                                value={q.answer ?? null}
-                                onChange={e => handleInputChange(index, e)}
-                                sx={{ display: "flex", minHeight: "100%", justifyContent: "space-around", marginLeft: 2 }}>
 
-                                <FormControlLabel value="A" control={<Radio
-                                    sx={{ color: `${q.id === inputError.id ? inputError.answer ? 'red' : '' : ''}`, '&.Mui-checked': { color: "green" }, }} />}
-                                    label="A" />
+                            </div>
 
-
-                                <FormControlLabel value="B" control={<Radio
-                                    sx={{ color: `${q.id === inputError.id ? inputError.answer ? 'red' : '' : ''}`, '&.Mui-checked': { color: "green" }, }} />}
-                                    label="B" />
-
-                                <FormControlLabel value="C" control={<Radio
-                                    sx={{ color: `${q.id === inputError.id ? inputError.answer ? 'red' : '' : ''}`, '&.Mui-checked': { color: "green" }, }} />}
-                                    label="C" />
-
-                            </RadioGroup>
-                        </FormControl> */}
                         </Box>
 
                     </Box>
@@ -408,7 +435,7 @@ const Quizbuilder = () => {
 
 
 
-
+                {/* // ===== Quiz Form  ========= // */}
                 {
                     qArr.map((q, index) => {
 
@@ -516,12 +543,16 @@ const Quizbuilder = () => {
                 {/* Action buttons for Add & Finish */}
 
                 < Stack sx={{ marginTop: 6 }} direction="row" spacing={4} justifyContent="space-between" >
-                    <Fab color="primary" aria-label="add" onClick={handleAddFields} >
+                    <Fab color="primary" aria-label="add" onClick={handleAddFields} sx={{ visibility: currentQ === "title" ? "hidden" : "visible" }}>
                         <Add />
                     </Fab>
 
-                    <Fab aria-label="add" variant='extended' onClick={handelFinish} >
-                        Finish
+                    <Fab aria-label="add" variant='extended' onClick={handelFinish}
+                    // sx={{ visibility: started ? "visible" : currentQ === "title" && !started ? "hidden" : "visible" }} 
+                    >
+
+                        {!started ? "Next" : "Finish"}
+
                     </Fab>
                 </Stack>
 
