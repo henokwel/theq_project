e2c0812a6478ea3d90e9cc98f1863a7571d1828d
henokwel <henokwel@outlook.com>
2022-01-03 18:33:34 +0100

    taking q finished

--- pages/quizarena/[id].js
+++ pages/quizarena/[id].js
@@ -1,15 +1,14 @@
 
 import React, { useContext, useEffect, useState } from 'react'
 import {
-    ToggleButton, ToggleButtonGroup, Breadcrumbs, Link as M_Link, Typography, InputLabel, Select, MenuItem, FormHelperText, Divider
+    ToggleButton, ToggleButtonGroup, Breadcrumbs, Link as M_Link, Typography, Divider
 } from '@mui/material'
 
 
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
 import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import InputAdornment from '@mui/material/InputAdornment'
+
 
 import Radio from '@mui/material/Radio'
 import FormControl from '@mui/material/FormControl'
@@ -20,15 +19,22 @@ import Fab from '@mui/material/Fab'
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+
 import DialogTitle from '@mui/material/DialogTitle';
 
 import { useRouter } from 'next/router'
 
 import { Add, Delete, Title } from '@mui/icons-material';
-import NextLink from 'next/link';
-import { QuizUpdateContext, QuizStateContext } from '../../src/context'
+
+import { QuizStateContext, QuizUpdateContext } from '../../src/context'
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const uniqid = require("uniqid");
 
@@ -38,12 +44,14 @@ const QuizArena = () => {
     const { id } = route.query
 
     const res = useContext(QuizStateContext)
-    useEffect(() => {
-        const filterQuiz = res.filter(item => item.id === id)[0]
-        console.log(filterQuiz.quiz);
+    // useEffect(() => {
+    //     const filterQuiz = res.filter(item => item.id === id)[0]
+    //     console.log(filterQuiz.quiz);
 
-        setQArr(filterQuiz.quiz)
-    }, [])
+    //     setQArr(filterQuiz.quiz)
+    // }, [])
+
+
 
 
     const [currentQ, setCurrentQ] = useState("title") // Toggle whick quiz Question you are working on
@@ -104,6 +112,43 @@ const QuizArena = () => {
         console.log(qArr);
         // setAlignment(newAlignment);
     };
+
+
+
+
+
+    useEffect(() => {
+        (async () => {
+
+
+
+            const filterQuiz = await res.filter(item => item.id === id)[0]
+            console.log(filterQuiz.quiz);
+
+
+            handleAlignment(null, filterQuiz.quiz[0].id)
+
+            setQArr(filterQuiz.quiz)
+
+
+
+            /// Save data to Local, to avoid error
+
+            if (filterQuiz.quiz === undefined) {
+                route.push("/")
+                return
+            }
+        })()
+
+
+    }, [])
+
+
+
+
+
+
+
 
 
 
@@ -244,47 +289,20 @@ const QuizArena = () => {
 
     const handelFinish = async () => {
 
-        // Handle Started state ON start &  ? On Finish ?
 
 
-        if (qInfo.title === "") {
+        // hide Quiz arena => animation, celebration
 
-            return setInfoError(true)
+        setHideQ_Arena(true)
 
-        } else if (!started) {
-            console.log("Run handleFinsih");
-            setStarted(true)
-            setInfoError(false) // title is given
-            handleAlignment(null, qArr[0].id)
-        } else {
+        // calculate answers
+        // display Results 
+        // map & show q and alt with right answers vs users answers
 
-            // !!! This is a hack. I need to find a better way of doing this part
 
-            const checkFinish = handleAddFields()
 
-            if (checkFinish) {
 
-                // run Add Function and check for error. 
-                // if no error, remove the last obj
 
-                // Filter the last Empty Quiz out
-                const finishedQuizz_arr = qArr.filter(item => item.id !== currentQ)
-
-                // if (qInfo.title === "") {
-                //     qInfo.title = `Quiz _${qInfo.id}`
-                // }
-
-                _quizContextUpdate({
-                    type: "add_quiz",
-                    payload: {
-                        ...qInfo,
-                        quiz: qArr.length > 1 ? finishedQuizz_arr : qArr
-                    }
-                })
-
-                router.push("/")
-            }
-        }
     }
 
 
@@ -292,7 +310,7 @@ const QuizArena = () => {
     return (
         <>
             <CssBaseline />
-            <Container maxWidth="md">
+            <Container maxWidth="md" >
 
 
                 <div role="presentation" onClick={handleBreadCrumb} style={{ display: "flex", justifyContent: "flex-end" }}>
@@ -327,7 +345,7 @@ const QuizArena = () => {
                          </ToggleButton> */}
 
                         {
-                            qArr.map((item, index) => <ToggleButton sx={{ visibility: started ? "visible" : "hidden" }} key={item.id} value={item.id} aria-label="centered">
+                            qArr.map((item, index) => <ToggleButton key={item.id} value={item.id} aria-label="centered">
                                 <h3 style={{ margin: 0 }}>{`Q${index + 1}`}</h3>
                             </ToggleButton>
                             )
@@ -375,7 +393,7 @@ const QuizArena = () => {
                             <Box sx={{ m: 2, display: q.id === currentQ ? "block" : "none" }} key={`${q}~${index}`}  >
 
                                 {/* Question Input */}
-                                <div style={{ display: "flex" }}>
+                                <div>
                                     {/* <TextField
                                         id="standard-multiline-flexible"
                                         name="q_"
@@ -394,7 +412,7 @@ const QuizArena = () => {
                                     /> */}
 
 
-                                    <Typography variant='h3'>
+                                    <Typography variant='h4' sx={{ mt: 5 }}>
                                         {q.q}
                                     </Typography>
 
@@ -411,16 +429,7 @@ const QuizArena = () => {
                                     {/* Multiple choice */}
 
                                     <Stack spacing={3} sx={{ minWidth: "40%" }}>
-                                        {/* <TextField
-                                            id="standard-multiline-flexible"
-                                            placeholder="Enter choose A"
-                                            name="altA"
-                                            // error={q.id === inputError.id ? inputError.alt : false}
-                                            value={q.altA}
-                                            variant="standard"
-                                            disabled
-                                        // onChange={e => handleInputChange(index, e)}
-                                        /> */}
+
                                         <Typography variant='p' si>
                                             {q.altA}
                                         </Typography>
@@ -434,27 +443,7 @@ const QuizArena = () => {
                                             {q.altC}
                                         </Typography>
                                         <Divider />
-                                        {/* <TextField
-                                            id="standard"
-                                            placeholder="Enter choose B "
-                                            name="altB"
-                                            // error={q.id === inputError.id ? inputError.alt : false}
-                                            value={q.altB}
-                                            variant="standard"
-                                            disabled
-                                        // onChange={e => handleInputChange(index, e)}
-                                        />
 
-                                        <TextField
-                                            id="standard"
-                                            placeholder="Enter choose C "
-                                            name="altC"
-                                            // error={q.id === inputError.id ? inputError.alt : false}
-                                            value={q.altC}
-                                            // onChange={e => handleInputChange(index, e)}
-                                            variant="standard"
-                                            disabled
-                                        /> */}
                                     </Stack>
 
                                     {/* Answer */}
@@ -500,7 +489,7 @@ const QuizArena = () => {
                     </Fab> */}
 
                     <Fab aria-label={!started ? "Next" : "Finish"} variant='extended' onClick={handelFinish}
-                        sx={{ visibility: !started ? "visible" : currentQ === "title" && started ? "hidden" : "visible" }}
+                    // sx={{ visibility: !started ? "visible" : currentQ === "title" && started ? "hidden" : "visible" }}
                     >
                         {!started ? "Next" : "Finish"}
                     </Fab>
